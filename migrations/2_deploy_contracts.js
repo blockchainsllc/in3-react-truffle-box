76b553e47dff5f429bf25fb75bdef55a7e14a288
Muhammad-Altabba <muhammad.altabba@slock.it>
2019-12-17 13:57:36 +0100

    Tiny edit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,14 @@
 // In this Truffle Box sample, for simplicity, a deployment of a smart contract is not included.
-//  However, you can simply uncomment the commented code and create a solidity Smart Contract at ./SmartContractCode.sol
+//  However, you can simply uncomment the commented code and create a solidity Smart Contract for example at ./SmartContractCode.sol
 
 // var SmartContractArtifacts = artifacts.require("./SmartContractCode.sol");
 
 module.exports = function(deployer) {
-  console.log("Nothing to deploy in this sample. The purpose of this sample is to show how to get trusted information, from Ethereum network, without the need to run a full Ethereum Node.");
+  console.log("Nothing to deploy in this sample.");
+  
+  console.log("The purpose of this sample is to show how to get trusted information, from Ethereum network, without the need to run a full Ethereum Node.");
 
   console.log("Feel free to add and deploy your contracts if needed.");
-  
+
   // deployer.deploy(SmartContractArtifacts);
 };
