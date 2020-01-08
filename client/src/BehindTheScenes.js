d0cb157852b38f3c0ac63d795627f89934018171
Muhammad-Altabba <muhammad.altabba@slock.it>
2020-01-08 11:44:34 +0100

    Few edits and Added a link to How Incubed Works

--- client/src/BehindTheScenes.js
+++ client/src/BehindTheScenes.js
@@ -11,8 +11,7 @@ class BehindTheScenes extends React.Component {
               What Happens Behind the Scenes!
             </h3>
             <p>
-              This occurs for every request from the Ethereum network. To understand more, you can open the Network tab of the Browser Developer Tools and check the request and response.
-                        {/* Add a link to How Incubed Works website once it is publicly ready! */}
+              This occurs for every request from the Ethereum network. To use IN3 you do not need to know all those details. But knowing them will give you confidence in this technical invention.
             </p>
             <ul>
               <li>Incubed Client injectes an <strong>"in3"</strong> property at the JSON request.</li>
@@ -23,6 +22,7 @@ class BehindTheScenes extends React.Component {
               <li>Incubed Client receives the data from all the inquired servers, and validates all the responses.</li>
               <li>Incubed Client validates all the proof of the validatable-data received from each server.</li>
             </ul>
+            <p>To understand more, you can open the Network tab of the Browser Developer Tools and check the request and response.</p>
             <p>Note: Put into consideration that in addition to the provided proofs and signatures, Incubed Servers (Nodes) cannot provide fake signatures since there are always <strong>`watch-dogs`</strong> who will be pleased to get the node's deposits in such a case (anyone can act as a watchdog!).</p>
           </div>
           <div>
@@ -36,15 +36,15 @@ class BehindTheScenes extends React.Component {
           </div>
           <div>
             <p>
-              <span>
-                By using Incubed as a provider for Web3, you can still call all the methods of Web3 as usual.
-              </span>{" "}
-              <span>
-                For more, {" "}
-                <a className="text-gray-dark font-weight-700"
+              <br />
+              By using Incubed as a provider for Web3, you can still call all the methods of Web3 as usual.
+              {" "}
+              <br />
+              <b>For more, check the Incubed Verification video, the examples, and the Play Ground at
+                {" "} <a className="text-gray-dark font-weight-700"
                   target="_blank" rel="noopener noreferrer"
-                  href="https://in3.readthedocs.io/en/latest/">read the Docs</a>.
-              </span>
+                  href="https://how-in3-works.slock.it/">How Incubed Works</a>.
+              </b>
             </p>
           </div>
         </div>
